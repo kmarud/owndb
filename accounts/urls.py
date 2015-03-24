dce258687175867f75660d5abf23a1072bd4c221
ofilipowicz <oskar.filipowicz@gmail.com>
2015-03-24 01:56:26 +0100

    login is working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from accounts.views import RegistrationView
+from accounts.views import login_user
 
 
 urlpatterns = patterns('',
                        url(r'^/?$', RegistrationView.as_view(), name='registraionview-list'),
-                       url(r'^login$', 'django.contrib.auth.views.login', {'next_page': '/store'}),
+                       url(r'^login$', login_user, name='login'),
                        url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/'}),
                        )
